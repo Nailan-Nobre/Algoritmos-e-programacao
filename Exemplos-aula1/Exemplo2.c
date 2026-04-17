4fbca80c314c3b41945519fdc8e2c48edb79dfba
Nailan-Nobre <nailannobre22@gmail.com>
2026-04-16 21:28:32 -0300

    Primeiro commit

---
+++ Exemplos-aula1/Exemplo2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b, c;
+
+    a = 7 + 9;
+    b = a + 10;
+    c = b - a;
+
+    printf("O valor de a = %d, b = %d e c = %d", a, b, c);
+}
---
+++ Exemplos-aula1/Exempo3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c, d;
+
+    d = 3;
+    c = 2;
+    b = 4;
+    d = c + b;
+    a = d + 1;
+    a = a + 1;
+
+    printf("O valor de a = %d, b = %d, c = %d e d = %d", a, b, c, d);
+}
